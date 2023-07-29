6eaef7fcc55e63c2380c19637feedcfbd3cf4cfb
tobias <tobias.omondi@student.moringaschool.com>
2023-07-29 23:01:36 +0300

    my-assignment

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seen = new Set();
+
+  for (const num of array) {
+    const diff = target - num;
+    if (seen.has(diff)) {
+      return true;
+    }
+    seen.add(num);
+  }
+
+  return false;
 }
 
 /* 
@@ -8,10 +19,19 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+   Create an empty hash set called "seen".
+   For each num in the array:
+   Add num to "seen".
+  If the loop completes without finding a pair, return false.
 */
 
 /*
   Add written explanation of your solution here
+ function uses a hash set to keep track of the numbers it has seen so far while traversing the array.
+ calculates the difference between the target and the current number.(each array)
+ it adds the current number to the set and continues to the next number in the array.(not present)
+  If the loop completes without finding a pair, the function returns false.
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +49,18 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([5, 5, 5, 5], 10));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([0, 0, 0, 0], 1));
 }
+
+module.exports = hasTargetSum;
+
 
 module.exports = hasTargetSum;
